39c3769f8128a0722dc5e8db81ce4e4bee23bc4d
Anthony Colaiacovo <anthonyecolaiacovo@gmail.com>
2024-11-06 16:34:06 -0500

    Add tag type deduction

--- client/src/classes/Tag.jsx
+++ client/src/classes/Tag.jsx
@@ -1,7 +1,28 @@
+const tagType = {
+    closing : 0,
+    opening : 1,
+    unknown : 2
+}
+
 class Tag {
     constructor(tag) {
         this.tag = tag;
+        this.type = this.determineType();
     }
+
+    determineType() {
+        const t = this.tag; 
+        let type = tagType.unknown;
+
+        if (t.startsWith("</")) {
+            type = tagType.closing;
+        } else if (t.startsWith("<")) {
+            type = tagType.opening;
+        } 
+            
+        return type;
+    }
+
 }
 
 export default Tag;
